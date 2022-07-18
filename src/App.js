f5b30c3d62b05915bf1b38d0678f7f694d10a5a0
varun kumar <varun2907@gmail.com>
2022-07-18 14:20:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import Signup from './components/Signup';
+import {BrowserRouter as Router , Switch , Route} from 'react-router-dom'
+import Login from './components/Login';
+import Landing from './components/Landing';
+import Addtask from './components/Addtask';
+import Tasklist from './components/Tasklist';
+import Edit from './components/Edit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+
+          <Route exact path="/">
+            <Landing/>
+          </Route>
+
+          <Route path="/login">
+            <Login/>
+          </Route>
+
+          <Route path="/signup">
+            <Signup/>
+          </Route>
+
+          <Route path="/addtask">
+            <Addtask/>
+          </Route>
+
+          <Route path="/tasklist">
+            <Tasklist/>
+          </Route>
+
+          <Route path="/edittask:id">
+            <Edit/>
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
